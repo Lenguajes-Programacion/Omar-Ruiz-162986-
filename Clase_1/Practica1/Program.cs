e4d95b45ca6f9c5c262cdf6e03fb7abfd0277348
Omar Ruiz <60107784+OmarRuiz9803@users.noreply.github.com>
2020-01-22 12:59:02 -0700

    Hola Mundo

---
+++ Clase_1/Practica1/Program.cs
@@ -0,0 +1,11 @@
+using System; //importarmos la libreria principal
+namespace Practica_1
+{
+    class Program
+    {
+        static void Main (string args)
+        {
+            Console.WriteLine("Hola Mundo!!!!!");
+        }
+    }
+}
