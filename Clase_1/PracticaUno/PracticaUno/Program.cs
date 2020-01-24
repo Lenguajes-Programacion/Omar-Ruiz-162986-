c9edf056b52c81a3ffbf2f5ae861e65c44d02dcf
Omar Ruiz <60107784+OmarRuiz9803@users.noreply.github.com>
2020-01-24 12:54:54 -0700

    Practica Y cambios

---
+++ Clase_1/PracticaUno/PracticaUno/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; //importarmos la libreria principal
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1
+{
+   public class Program
+    {
+
+        static void Main(string[] args) //primera funcion que correra
+        {
+            //string nombre = "Omar R";
+            Console.WriteLine($"Hola {args[0]}!");
+            Console.ReadKey();
+        }
+    }
+}
