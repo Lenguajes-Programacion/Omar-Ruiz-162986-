10d332fca001e070595f764873f9c347a4efeeda
Omar Ruiz <60107784+OmarRuiz9803@users.noreply.github.com>
2020-01-24 00:26:44 -0700

    Update Program.cs

--- Clase_1/Practica1/Program.cs
+++ Clase_1/Practica1/Program.cs
@@ -1,13 +1,17 @@
 using System; //importarmos la libreria principal
-
+using System.collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Practica_1
 {
     class Program
     {
-        static void Main (string args)
+        static void Main (string[] args)
         {
             Console.WriteLine("Hola Mundo!!!!!");
+            Console.ReadKey();
         }
     }
 }
