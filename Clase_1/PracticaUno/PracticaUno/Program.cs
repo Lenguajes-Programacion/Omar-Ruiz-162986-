b1a6f2ee32aac76e526d8d89c4a387a623712326
Omar Ruiz <60107784+OmarRuiz9803@users.noreply.github.com>
2020-01-27 12:24:19 -0700

    Proyecto Terminado

--- Clase_1/PracticaUno/PracticaUno/Program.cs
+++ Clase_1/PracticaUno/PracticaUno/Program.cs
@@ -1,19 +1,28 @@
 ﻿using System; //importarmos la libreria principal
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Practica_1
 {
    public class Program
     {
-
+       static string name = "omar";
         static void Main(string[] args) //primera funcion que correra
         {
-            //string nombre = "Omar R";
-            Console.WriteLine($"Hola {args[0]}!");
+            //condicion se debe de cumplir las sentencias que utilizamos &&
+            //condicion se debe de cumplir las sentencias que utilizamos ||
+            // >< == = 
+            if (args[0].Length > 0 && args[0] == name)
+            {
+                Console.WriteLine($"Hello {args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("saquese");
+            }
+
+            
             Console.ReadKey();
         }
+       
     }
 }
