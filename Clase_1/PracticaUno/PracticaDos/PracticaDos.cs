3990ff1104e3b056a71fc46fe5189b351c297da5
Omar Ruiz <60107784+OmarRuiz9803@users.noreply.github.com>
2020-02-12 12:30:26 -0700

    Calculadora

--- Clase_1/PracticaUno/PracticaDos/PracticaDos.cs
+++ Clase_1/PracticaUno/PracticaDos/PracticaDos.cs
@@ -1,57 +1,90 @@
-﻿using System;
-namespace PracticaDos
-{
-    class PracticaDos
-    {
-        public static void Main(string[] args)
-        {
-            int Valor1;
-            int Valor2;
-            int resultado;
-            string continuar = null;
+﻿                            using System;
+                            namespace PracticaDos
+                            {
+                                class PracticaDos
+                                {
+                                    public static void Main(string[] args)
+                                    {
+                                        int Valor1;
+                                        int Valor2;
+                                        int resultado;
+                                        int resultado1 = 0;
+                                        int resultado2 = 0;
+                                        string continuar1 = null;
+                                        string continuar2 = null;
+           
+            
+            
 
-            Console.WriteLine("Ingrese el primer numero");
-            Valor1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Ingrese el segundo numero");
-            Valor2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Ingrese una opcion en la calculadora");
-            Console.WriteLine("(+) para sumar");
-            Console.WriteLine("(-) para restar");
-            Console.WriteLine("(*) para Multiplicar");
-            Console.WriteLine("(S) para salir");
-            string opcion = Console.ReadLine();
+                                    Console.WriteLine("Ingrese el primer numero");
+                                    Valor1 = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine("Ingrese el segundo numero");
+                                    Valor2 = Convert.ToInt32(Console.ReadLine());
 
-            do
-            {
-                switch (opcion)
-                {
-                    case "+":
-                        Console.WriteLine("el resultado de la suma es:");
-                        resultado = Valor1 + Valor2;
-                        Console.WriteLine("{0} + {1} = {2}", Valor1, Valor2, resultado);
-                        break;
-                    case "-":
-                        Console.WriteLine("el resultado de la resta es:");
-                        resultado = Valor1 - Valor2;
-                        Console.WriteLine("{0} - {1} = {2}", Valor1, Valor2, resultado);
-                        break;
-                    case "*":
-                        Console.WriteLine("el resultado de la suma es:");
-                        resultado = Valor1 * Valor2;
-                        Console.WriteLine("{0} * {1} = {2}", Valor1, Valor2, resultado);
-                        break;
-                    case "S":
-                        Console.WriteLine("hasta la proxima");
-                        Console.ReadKey();
-                        break;
+                                    Console.WriteLine("Ingrese una opcion en la calculadora");
+                                    Console.WriteLine("(+) para sumar");
+                                    Console.WriteLine("(-) para restar");
+                                    Console.WriteLine("(*) para Multiplicar");
+                                    Console.WriteLine("(S) para salir");
+                                    string opcion = Console.ReadLine();
 
-                }
-                Console.WriteLine("Desea Continuar con la calculadora? Si/No");
-                continuar = Console.ReadLine();
+                                    do
+                                    {
+                                        switch (opcion)
+                                        {
+                                            case "+":
+                                                Console.WriteLine("el resultado de la suma es:");
+                                                resultado = Valor1 + Valor2;
+                                                Console.WriteLine("{0} + {1} = {2}", Valor1, Valor2, resultado);
+                                            Console.WriteLine("Desea Utilizar el resultado para una otra operacion? si/no");
+                                          continuar2 = Console.ReadLine();
+                                           _ = continuar2 == "si";
+                                           Console.WriteLine("El resultado anterior es:" + resultado + "\n");
+                            Console.WriteLine("Agregue un nuevo dato para sumar");
+                            resultado1 = Convert.ToInt32(Console.ReadLine());
+                            resultado2 = resultado + resultado1;
+                            Console.WriteLine("el resultado es:" + resultado2);
+                                            break;
+                                            case "-":
+                                                Console.WriteLine("el resultado de la resta es:");
+                                                resultado = Valor1 - Valor2;
+                                                Console.WriteLine("{0} - {1} = {2}", Valor1, Valor2, resultado);
+                            Console.WriteLine("Desea Utilizar el resultado para una otra operacion? si/no");
+                            continuar2 = Console.ReadLine();
+                            _ = continuar2 == "si";
+                            Console.WriteLine("El resultado anterior es:" + resultado + "\n");
+                            Console.WriteLine("Agregue un nuevo dato para restar");
+                            resultado1 = Convert.ToInt32(Console.ReadLine());
+                            resultado2 = resultado - resultado1;
+                            Console.WriteLine("el resultado es:" + resultado2);
+                            break;
+                                            case "*":
+                                                Console.WriteLine("el resultado de la multiplicacion es:");
+                                                resultado = Valor1 * Valor2;
+                                                Console.WriteLine("{0} * {1} = {2}", Valor1, Valor2, resultado);
+                            Console.WriteLine("Desea Utilizar el resultado para una otra operacion? si/no");
+                            continuar2 = Console.ReadLine();
+                            _ = continuar2 == "si";
+                            Console.WriteLine("El resultado anterior es:" + resultado + "\n");
+                            Console.WriteLine("Agregue un nuevo dato para multiplicar");
+                            resultado1 = Convert.ToInt32(Console.ReadLine());
+                            resultado2 = resultado * resultado1;
+                            Console.WriteLine("el resultado es:" + resultado2);
+                            break;
+                                            case "S":
+                                                Console.WriteLine("hasta la proxima");
+                                                Console.ReadKey();
+                                                break;
 
-            } while (continuar == "Si" || continuar == "si");
-            Console.ReadKey();
-        }
-    }
-}
+                                        }
+                          
+
+                                        Console.WriteLine("Desea Continuar con la calculadora? Si/No");
+                                        continuar1 = Console.ReadLine();
+
+                                    } while (continuar1 == "Si" || continuar1 == "si");
+                                       Console.ReadKey();
+                                }
+                            }
+                        }
