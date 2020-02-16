f6cc2502d6a33e1b48a2ca644f48b43e1d2a9d4f
Omar Ruiz <60107784+OmarRuiz9803@users.noreply.github.com>
2020-02-16 11:30:26 -0700

    0001

--- EntregaUno162986/EntregaUno162986/ClassCalcu.cs
+++ EntregaUno162986/EntregaUno162986/ClassCalcu.cs
@@ -8,5 +8,8 @@ namespace EntregaUno162986
 {
     class ClassCalcu
     {
+        int Valor1; int Valor2; int resultado;
+        int resultado1 = 0; int resultado2 = 0;
+        string continuar1 = null; string continuar2 = null;
     }
 }
