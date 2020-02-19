0c8b94fe25ebc372bec6517ea0f78cd937abdd4f
JR Saucedo <betoflakes@gmail.com>
2020-02-19 12:24:43 -0700

    Entrega 100

--- EntregaUno162986/EntregaUno162986/Program.cs
+++ EntregaUno162986/EntregaUno162986/Program.cs
@@ -3,7 +3,7 @@
             using System.Linq;
             using System.Text;
             using System.Threading.Tasks;
-
+// Entrega 100
     namespace EntregaUno162986
     {
         public class Program
