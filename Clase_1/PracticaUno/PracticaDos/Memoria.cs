7019056c39886ba467bade4c74bc1b8e655e7e69
Omar Ruiz <60107784+OmarRuiz9803@users.noreply.github.com>
2020-02-21 11:42:29 -0700

    Version2

---
+++ Clase_1/PracticaUno/PracticaDos/Memoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace PracticaDos
+{
+    class Memoria
+    {
+        public void GuardadMemoria()
+        {
+            string ArchivoDB = "../db.json";
+            StreamReader reader = new StreamReader(ArchivoDB);
+            var dbJSON = reader.ReadToEnd();
+            var dbObject = JObject.Parse(dbJSON);
+            
+
+            
+        }
+    }
+}
